982cf8bbe062b87e70883b7211d2f2bd6479f96f
Rafal <zgoda.rafal@gmail.com>
2022-08-17 22:13:40 +0200

    add private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,12 @@
 import { task, HardhatUserConfig } from 'hardhat/config';
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-waffle';
-
-import deployer from './.secret';
+import 'dotenv/config';
 
 // const BSC_RPC = 'https://bsc-dataseed.binance.org/';
 const BSC_RPC = 'https://bsc-dataseed1.defibit.io/';
 const BSC_Tetsnet_RPC = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
-
+if (!process.env.PRIVATE_KEY) throw new Error('Missing PRIVATE_KEY');
 const config: HardhatUserConfig = {
   solidity: { version: '0.7.6' },
   networks: {
@@ -24,12 +23,12 @@ const config: HardhatUserConfig = {
     bscTestnet: {
       url: BSC_Tetsnet_RPC,
       chainId: 0x61,
-      accounts: [deployer.private],
+      accounts: [process.env.PRIVATE_KEY],
     },
     bsc: {
       url: BSC_RPC,
       chainId: 0x38,
-      accounts: [deployer.private],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
   mocha: {
